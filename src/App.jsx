a3128b3b132a33f675b9a4e84cb2430e2a9cd52c
Diego Cuevas <ingdcuevas@gmail.com>
2023-03-10 16:44:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import { Container } from "./components/styles/Container.styled";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Hola Mundo</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./styles/Header.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <h1 className="prueba">Hubble</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
